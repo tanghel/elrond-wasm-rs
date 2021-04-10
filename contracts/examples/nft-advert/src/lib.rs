79901e83023afdd076f4d744e1351405bd5ffae6
Traian Anghel <tanghel@live.com>
2021-04-10 22:23:42 +0300

    first function for getting price of nft advert

--- contracts/examples/nft-advert/src/lib.rs
+++ contracts/examples/nft-advert/src/lib.rs
@@ -5,6 +5,9 @@ mod nft_advert;
 use nft_advert::Advert;
 
 elrond_wasm::imports!();
+elrond_wasm::derive_imports!();
+
+const ONE_EGLD: u64 = 1000000000000000000u64;
 
 /// Multi-signature manager implementation.
 /// Associated names to multisig smart contracts, as well as multisg address lists for wallet addresses.
@@ -14,17 +17,16 @@ pub trait NftAdvert {
 	fn get_adverts(&self) -> MapMapper<Self::Storage, u32, Advert>;
 
 	#[view(getPrice)]
-	fn get_price(&self, index: u8) -> u32 {
+	fn get_price(&self, index: u8) -> BigUint {
 		if index >= 245 {
-			return 0u32;
+			return BigUint::zero();
 		}
 
-		let multiplier = if index < 5 { 16.0 } else { if index < 65 { 4.0 } else { 1.0 } };
+		let multiplier = if index < 5 { 16 } else { if index < 65 { 4 } else { 1 } };
 		let box_index = index % 5;
 
-		let number_of_pixels = multiplier * if box_index < 3 { 2500.0 } else { 1250.0 };
-		let price_per_pixel = 0.008;
+		let number_of_pixels = multiplier * if box_index < 3 { 2500 } else { 1250 };
 
-		return (number_of_pixels * price_per_pixel) as u32;
+		return BigUint::from(ONE_EGLD).mul(BigUint::from(number_of_pixels as u32)).mul(BigUint::from(8u32)).div(BigUint::from(1000u32));
 	}
 }
